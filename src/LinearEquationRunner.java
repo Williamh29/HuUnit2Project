ccc83cdbdf7cb8b7efe387150868e4efad2e4aa7
student <williamh29@nycstudents.net>
2022-10-17 10:54:56 -0400

    initialize

---
+++ src/LinearEquationRunner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class LinearEquationRunner {
+    public static void main (String [] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome!");
+        System.out.print("Enter coordinate 1: ");
+        int x1 = scan.nextInt();
+        System.out.print("Enter coordinate 2: ");
+        int y1 = scan.nextInt();
+        System.out.print(" ");
+    }
+}
